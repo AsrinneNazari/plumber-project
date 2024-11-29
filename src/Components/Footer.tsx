381f3fceecd51b389190e3f5b1ea5b3839a100fa
Asrinne Nazari <asse_89@hotmail.com>
2024-11-29 11:17:30 +0100

    add footer component and some styling

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom"
+
+export const Footer = () =>{
+
+    return <footer className="border-t-4 border-slate-200">
+    <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+            <div className="px-5 py-2">
+                <NavLink to="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Hem
+                </NavLink>
+            </div>
+            <div className="px-5 py-2">
+            <NavLink to="/about" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Om oss
+                </NavLink>
+            </div>
+            <div className="px-5 py-2">
+            <NavLink to="/services" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Tjänster
+                </NavLink>
+            </div>
+            <div className="px-5 py-2">
+            <NavLink to="/contact" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+                    Kontakta oss
+                </NavLink>
+            </div>
+        </nav>
+        <p className="mt-8 text-base leading-6 text-center text-gray-400">
+            © 2024 Mello Rör AB, Inc. All rights reserved.
+        </p>
+    </div>
+    </footer>
+}
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 export const Layout = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <Footer />
     </>
   );
 };
