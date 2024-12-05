e0844922313c61b09b29e15a0d320c9315848997
Asrinne Nazari <asse_89@hotmail.com>
2024-12-05 14:07:52 +0100

    Update logo font

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -3,6 +3,6 @@ import { NavLink } from "react-router-dom"
 export const Logo = () =>{
     /* Future logo here! */
 return <>
-<NavLink to="/">Mello Rör AB</NavLink>
+<NavLink to="/" className="font-roboto font-bold text-2xl w-full">Mello Rör AB</NavLink>
 </>
 }
