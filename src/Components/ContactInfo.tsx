84ddcc35af0609fc7b428ac543e2f2ee14de1613
Asrinne Nazari <asse_89@hotmail.com>
2024-12-09 11:58:39 +0100

    create contactInfo component

---
+++ src/Components/ContactInfo.tsx
@@ -0,0 +1,12 @@
+export const ContactInfo = () => {
+  return (
+    <>
+      <div className="flex-[1] border-l-2 border-lightgray pl-9 pt-10 order-1 lg:order-2">
+        <p className="font-semibold font-fjalla text-lx mb-3 text-2xl">
+          Kontakta oss direkt
+        </p>
+        <p className="font-bold">Telefonnummer: 070-1234567</p>
+      </div>
+    </>
+  );
+};
--- src/Pages/Services.tsx
+++ src/Pages/Services.tsx
@@ -1,20 +1,26 @@
 import { IServiceContent } from "../models/IServiceContent";
 import { useSupabase } from "../hooks/useSupabase";
+import { ContactInfo } from "../components/ContactInfo";
 
 export const Services = () => {
   const [loading, data] = useSupabase<IServiceContent[]>("ServiceContent");
 
   return (
     <>
-      {loading && <h1>Loading..</h1>}
-      <div className="p-5 sm:p-5 mx-auto max-w-7xl flex flex-col">
+      <div className="p-5 sm:p-5 mx-auto max-w-7xl flex flex-col lg:flex-row">
+      <ContactInfo/>
+        <div className="flex-[2] order-2 lg:order-1">
         <div className="font-sans text-3xl p-4 font-medium">Våra tjänster</div>
+        <div className="max-w-xl">
+        {loading && <h1>Loading..</h1>}
         {data?.map((item, index) => (
-          <section className=" sm:p-3 mx-auto max-w-2xl p-1" key={index}>
+          <section className="mb-4" key={index}>
             <p className="font-bold"> {item.title}</p>
             {item.description}
           </section>
         ))}
+      </div>
+      </div>
       </div>
     </>
   );
