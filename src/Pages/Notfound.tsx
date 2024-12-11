6560fcc1e4a040ce99c900ed4a652b0d8fb0b8cf
Asrinne Nazari <asse_89@hotmail.com>
2024-12-11 11:06:30 +0100

    add styling to NotFound page

--- src/Pages/Notfound.tsx
+++ src/Pages/Notfound.tsx
@@ -1,3 +1,17 @@
+import { Button } from "../components/Button";
+import leakingPipe from "./../img/leakingPipe.png";
+
 export const Notfound = () => {
-  return <>:(</>;
+  return (
+    <>
+      <div className="p-5 sm:p-5 mx-auto max-w-3xl flex flex-col lg:flex-col gap-10">
+        <div className="flex items-center flex-col justify-center min-h-screen">
+          <h1 className="text-8xl">404</h1>
+          <h3 className="text-5xl pb-6">Page not found!</h3>
+          <img src={leakingPipe} alt="Leaking Pipe" className="w-80 m-10" />
+          <Button to="/" buttonText="Tillbaka till startsidan" />
+        </div>
+      </div>
+    </>
+  );
 };
