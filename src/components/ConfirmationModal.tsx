8eb82fda6904c91ee4540db6c41a54372572ba2f
Asrinne Nazari <asse_89@hotmail.com>
2025-01-02 15:53:10 +0100

    remove code

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -12,7 +12,7 @@ const ConfirmationModal = ({ show, onClose }: IModalProps) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
       <div className="bg-white p-9 rounded shadow-lg">
         <p>Meddelande skickat</p>
-        <p className="">Vi återkommer till dig så fort som möjligt!</p>
+        <p>Vi återkommer till dig så fort som möjligt!</p>
         <div className="mt-3">
           <Button click={onClose} buttonText="Stäng" />
         </div>
